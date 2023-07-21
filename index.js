9c1dceecdc10f9f4353e4ce49a64c91d65644d6c
Isis <135667009+isisb92@users.noreply.github.com>
2023-07-20 22:34:47 -0600

    clean JS code 50%

--- index.js
+++ index.js
@@ -1,106 +1,87 @@
-let now = new Date();
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let dayIndex = days[now.getDay()];
+  let monthIndex = months[now.getMonth()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let weatherDate = document.querySelector("#weatherCurrentDate");
-let dayIndex = now.getDay();
-let monthIndex = now.getMonth();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+  return `${days[dayIndex]} ${
+    months[monthIndex]
+  } ${now.getDate()}, ${now.getFullYear()} ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-weatherDate.innerHTML = `${days[dayIndex]} ${
-  months[monthIndex]
-} ${now.getDate()}, ${now.getFullYear()} ${hours}:${minutes}`;
 
 function showTemp(response) {
-  let temp = Math.round(response.data.main.temp);
-  let newTempCity = document.querySelector("#currentTemp");
-  newTempCity.innerHTML = `${temp}`;
-  let searchCityWeatherDescription = response.data.weather[0].description;
-  let weatherDescription = document.querySelector(
+  let tempElement = document.querySelector("#currentTemp");
+  let cityElement = document.querySelector("#result-city");
+  let descriptionElement = document.querySelector(
     "#current-weather-description"
   );
-  weatherDescription.innerHTML = `${searchCityWeatherDescription}`;
-  console.log(response.data.name);
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  let celsiusTemperature = response.data.main.temp;
+
+  tempElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function searchBtn(event) {
-  event.preventDefault();
-  let newSearchedCity = document.querySelector("#new-city-input");
-  let currentCity = document.querySelector("#result-city");
-  currentCity.innerHTML = `${newSearchedCity.value}`;
+function search(city) {
   let units = "metric";
   let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
-  let apiUrl = `${apiEndpoint}q=${newSearchedCity.value}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `${apiEndpoint}q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemp);
 }
 
-let newCity = document.querySelector("#search-city-form");
-newCity.addEventListener("submit", searchBtn);
-
-function changeF() {
-  let newTempF = document.querySelector("#currentTemp");
-  newTempF.innerHTML = "88";
-}
-let fTemp = document.querySelector("#f-unit");
-fTemp.addEventListener("click", changeF);
-
-function showTempCurrent(response) {
-  let tempCurrent = Math.round(response.data.main.temp);
-  let currentTempCity = document.querySelector("#currentTemp");
-  currentTempCity.innerHTML = `${tempCurrent}`;
-  let currentCityName = document.querySelector("#result-city");
-  currentCityName.innerHTML = response.data.name;
-  let currentWeatherDescription = response.data.weather[0].description;
-  let weatherDescriptionCurrent = document.querySelector(
-    "#current-weather-description"
-  );
-  weatherDescriptionCurrent.innerHTML = `${currentWeatherDescription}`;
-
-  console.log(response.data.name);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#new-city-input");
+  search(cityInputElement.value);
 }
 
-function showCurrentCity() {
-  navigator.geolocation.getCurrentPosition(showPosition);
-  function showPosition(position) {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-    let units2 = "metric";
-    let apiKey2 = "5aac6d0188c6f17d6d2bbe6591b6fef0";
-    let apiEndpoint2 = "https://api.openweathermap.org/data/2.5/weather?";
-    let apiUrl2 = `${apiEndpoint2}&lat=${lat}&lon=${lon}&appid=${apiKey2}&units=${units2}`;
-    axios.get(apiUrl2).then(showTempCurrent);
-  }
-}
-let myPlace = document.querySelector("#current-place");
-myPlace.addEventListener("click", showCurrentCity);
+let form = document.querySelector("#search-city-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Tegucigalpa");
